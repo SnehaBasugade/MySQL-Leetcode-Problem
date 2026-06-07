99bcfc7d22fc008b789ec8edee94f96f0a7c9d4c
SnehaBasugade <snehabasugade1111@gmail.com>
2026-06-07 09:18:10 -0600

    1141. User Activity for the Past 30 Days I
    Table: Activity
    
    +---------------+---------+
    | Column Name   | Type    |
    +---------------+---------+
    | user_id       | int     |
    | session_id    | int     |
    | activity_date | date    |
    | activity_type | enum    |
    +---------------+---------+
    This table may have duplicate rows.
    The activity_type column is an ENUM (category) of type ('open_session', 'end_session', 'scroll_down', 'send_message').
    The table shows the user activities for a social media website. 
    Note that each session belongs to exactly one user.
     
    
    Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
    
    Return the result table in any order.
    
    The result format is in the following example.
    
    Note: Any activity from ('open_session', 'end_session', 'scroll_down', 'send_message') will be considered valid activity for a user to be considered active on a day.
    
     
    
    Example 1:
    
    Input: 
    Activity table:
    +---------+------------+---------------+---------------+
    | user_id | session_id | activity_date | activity_type |
    +---------+------------+---------------+---------------+
    | 1       | 1          | 2019-07-20    | open_session  |
    | 1       | 1          | 2019-07-20    | scroll_down   |
    | 1       | 1          | 2019-07-20    | end_session   |
    | 2       | 4          | 2019-07-20    | open_session  |
    | 2       | 4          | 2019-07-21    | send_message  |
    | 2       | 4          | 2019-07-21    | end_session   |
    | 3       | 2          | 2019-07-21    | open_session  |
    | 3       | 2          | 2019-07-21    | send_message  |
    | 3       | 2          | 2019-07-21    | end_session   |
    | 4       | 3          | 2019-06-25    | open_session  |
    | 4       | 3          | 2019-06-25    | end_session   |
    +---------+------------+---------------+---------------+
    Output: 
    +------------+--------------+ 
    | day        | active_users |
    +------------+--------------+ 
    | 2019-07-20 | 2            |
    | 2019-07-21 | 2            |
    +------------+--------------+ 
    Explanation: Note that we do not care about days with zero active users.

--- DAY=28.sql
+++ DAY=28.sql
@@ -1,6 +1,5 @@
-SELECT *
-FROM cinema c
-WHERE c.id % 2 = 1
-    AND
-    c.description != 'boring'
-ORDER BY c.rating DESC;  
+SELECT activity_date AS day,
+       COUNT(DISTINCT user_id) AS active_users
+FROM activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date;
